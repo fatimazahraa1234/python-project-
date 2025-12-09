3b325d4d9cc2dbc56c6fa85642334380fa442e50
HP SPECTRE <aya.doubiani@uit.ac.ma>
2025-12-09 20:44:32 +0100

    I added data cleaning and basic statistics check this code before i continue with filtering and grouping data

--- DataAnalysisProject.py
+++ DataAnalysisProject.py
@@ -12,7 +12,7 @@
 
 #import needed modules in this project
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 #Build a class that will contain all methods to apply on dataset (load, analyze, plot )
 class DataAnalysisProject :
@@ -82,5 +82,85 @@ class DataAnalysisProject :
         print("\nMissing values in each column : ")
         #Cheks for Na values (empty or NULL values ) then sum the total number of missing values per column
         print(self.df.isnull().sum())
+
+    #----------------------------------------------------------------------------------------
+    #3-cleaning data: handling missing data,duplicate values and invalid entries
+    #----------------------------------------------------------------------------------------
+    def data_cleaning(self):
+        """Clean the dataset; remove rows with missing values,
+        remove duplicate rows,fix data of wrong format"""
+
+        # remove rows with missing values, apply changes to the original dataframe
+        self.df.dropna(inplace=True)
+        # remove duplicate rows
+        self.df=self.df.drop_duplicates()
+        # to reorder the index after dropping duplicate rows
+        self.df=self.df.reset_index(drop=True)
+
+        # convert tenure to numeric,coercing errors to NaN
+        self.df['tenure'] = pd.to_numeric(self.df['tenure'], errors='coerce')
+        #dropping rows where tenure is NaN
+        self.df.dropna(subset=['tenure'], inplace=True)
+        self.df.reset_index(drop=True)#reorder the index after dropping
+
+        #convert MonthlyCharges to numeric,coercing errors to NaN
+        self.df['MonthlyCharges']=pd.to_numeric(self.df['MonthlyCharges'], errors='coerce')
+        self.df.dropna(subset=['MonthlyCharges'], inplace=True) #dropping rows where monthly charges are NaN
+        self.df.reset_index(drop=True) #reset indexes after dropping
+
+        #convert TotalCharges to numeric,coercing errors to Nan
+        self.df['TotalCharges']=pd.to_numeric(self.df['TotalCharges'], errors='coerce')
+        self.df.dropna(subset=['TotalCharges'], inplace=True) #dropping rows with Nan values in total charges column
+        self.df.reset_index(drop=True) #reset indexing after dropping
+
+    #------------------------------------------------------------------------------------------------------------------
+    #4-Summarizing data:calculating basic statistics for numerical columns
+    #------------------------------------------------------------------------------------------------------------------
+    def data_statistics(self):
+        """Calculate and display statistics about the dataset:
+           average,max,min,mean,median,std"""
+
+        #max tenure
+        print("max tenure : ",self.df['tenure'].max())
+        #min tenure
+        print("min tenure : ",self.df['tenure'].min())
+        #average tenure
+        print("mean tenure : ",self.df['tenure'].mean())
+        #sorting data in ascending order
+        self.df.sort_values(by=['tenure'], inplace=True)
+        #median tenure
+        print("median tenure : ",self.df['tenure'].median())
+        #the standard deviation
+        print("std tenure : ",self.df['tenure'].std())
+
+        #max monthly charges
+        print("max monthly charges : ",self.df['MonthlyCharges'].max())
+        #min monthly charges
+        print("min monthly charges : ",self.df['MonthlyCharges'].min())
+        #average monthly charges
+        print("mean monthly charges : ",self.df['MonthlyCharges'].mean())
+        #sorting data in ascending order for monthly charges
+        self.df.sort_values(by=['MonthlyCharges'], inplace=True)
+        #median monthly charges
+        print("median monthly charges : ",self.df['MonthlyCharges'].median())
+        #standard deviation
+        print("std monthly charges : ",self.df['MonthlyCharges'].std())
+
+        #max total charges
+        print("max total charges : ",self.df['TotalCharges'].max())
+        #min total charges
+        print("min total charges : ",self.df['TotalCharges'].min())
+        #average total charges
+        print("mean total charges : ",self.df['TotalCharges'].mean())
+        #sorting values in ascending order (total charges)
+        self.df.sort_values(by=['TotalCharges'], inplace=True)
+        #median total charges
+        print("median total charges : ",self.df['TotalCharges'].median())
+        #standard charges
+        print("std total charges : ",self.df['TotalCharges'].std())
+
+    #-------------------------------------------------------------------------------------------------------------------
+    #5-Grouping and filtering data
+    #-------------------------------------------------------------------------------------------------------------------
 
 
