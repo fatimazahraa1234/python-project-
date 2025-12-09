bd52d3a61d0ec08c203133ade80295934deb22ff
fatimazahraa1234 <165699912+fatimazahraa1234@users.noreply.github.com>
2025-12-09 12:12:53 +0100

    Add files via upload

---
+++ DataAnalysisProject.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------
+#Digital Skills & Python Programming Final Project
+#-----------------------------------------------------------------
+#Team Members :
+"""
+    *** FATIMA ZAHRA EL HALFI ***
+    *** AYA DOUBIANI ***
+    *** IKRAM BEN AYA ***
+    *** OUMAIMA SAID ***
+    *** FATIMA ZAHRA EL-BOURIMI ***
+"""
+
+#import needed modules in this project
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class DataAnalysisProject :
+
