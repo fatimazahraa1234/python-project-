da2b4b40c158f8daa214ceef3f29adbaa844cf47
fatimazahraa1234 <fatielhalfi12@gmail.com>
2025-12-09 17:22:15 +0100

    Add data cleaning functions

--- DataAnalysisProject.py
+++ DataAnalysisProject.py
@@ -14,8 +14,73 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Build a class that will contain all methods to apply on dataset (load, analyze, plot )
 class DataAnalysisProject :
-   
+    def __init__(self,file_path):
+        #constructor : initializes the class with the path to the dataset file
+        #It stores the file's path to use it later in the class methods
+        self.file_path = file_path
+        #It creates an attribute called df (DataFrame) and sets it to empty at the beginning
+        self.df = None
 
+    #--------------------------------------------------
+    #1. Load Dataset
+    #--------------------------------------------------
+    def load_data(self):
+        """Load the data set from a csv file """
+        try :
+            #Read the csv file into a Data Frame for analysis
+            self.df = pd.read_csv(self.file_path)
+            print("File loaded successfully . ")
+        except FileNotFoundError :
+            #Error raised when the file does not exist
+            print("File not found . ")
+        except PermissionError :
+            #Error raised when the user has no permission to open the file
+            print("You don't have permission to access this file . ")
+        except Exception as e :
+            #Error raised when something went wrong while loading the csv file
+            print(f"Something went wrong while loading the file : {e} ")
+
+    #------------------------------------------------
+    #2. Inspect DataSet
+    #------------------------------------------------
+    def inspect_data(self):
+        """Inspect the data set
+        Displays dataset information , missing values
+        and basic statistics """
+        #Check if the data set is well loaded , if not it run out of the method to not cause errors
+        if self.df is None :
+            print("Dataset not loaded . ")
+            return #To stop the function
+
+        #summary of the dataframe with data types and missing values
+        print("\nDataset Information ******************** ")
+        print(self.df.info())
+
+        #Display the first 10 rows
+        print("\nFirst 10 rows : ")
+        print(self.df.head(10))
+
+        #Display the last 10 rows
+        print("\nLast 10 rows : ")
+        print(self.df.tail(10))
+
+        #Display the dataset's shape : tuple containing number of rows and columns
+        print("\nDataset's size is : ")
+        print(self.df.shape)
+
+        #List of columns along with data types
+        print("\nDataset columns and datatypes : ")
+        print(self.df.dtypes)
+
+        #Provide basic statistical information about numerical columns
+        print("\nDataset Description :")
+        print(self.df.describe())
+
+        #Identify and report missing values in each column
+        print("\nMissing values in each column : ")
+        #Cheks for Na values (empty or NULL values ) then sum the total number of missing values per column
+        print(self.df.isnull().sum())
 
 
